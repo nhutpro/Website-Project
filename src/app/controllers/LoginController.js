70cb1fed01fa3116514f00bcbf890b39dffe8cc8
nhutpro <tnhut803@gmail.com>
2021-10-09 15:54:32 +0700

    third commit

--- src/app/controllers/LoginController.js
+++ src/app/controllers/LoginController.js
@@ -1,12 +1,15 @@
-const Item = require('../models/Item')
+const items = require('../models/Item')
+
 class LoginController {
-    index(req, res) {
-    Item.find({}, function(err,itemlist){
-        if(!err) res.json(itemlist)
-        else
-        res.status(400).json({errro: 'ERROR !!!!'})
-    })
-   } 
+    index(req, res,next) {
+    items.find({})
+        .then(items => {
+            items = items.map((item) => item.toObject())
+            res.render('home',{items})
+        }
+        )
+        .catch(next)
+ }
    show(req, res){
        res.send('home' + req.params.id);
    }
