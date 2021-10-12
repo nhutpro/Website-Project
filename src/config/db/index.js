7d13d0f6c460cd93c99bdf9b6ea1d94e1bc59716
DESKTOP-KMNS09Q\Admin <19521589@gm.uit.edu.vn>
2021-10-12 21:43:48 +0700

    Delete trailing spaces

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose')
 async function connect() {
- try {
+try {
     await mongoose.connect('mongodb://localhost:27017/Website_13Team',{
         useNewUrlParser: true,
         useUnifiedTopology: true
     });
     console.log('Connect successfully!!!')
- } catch (error) {
-     console.log('faile !!!!')
- }
+} catch (error) {
+    console.log('faile !!!!')
+}
 }
 module.exports = { connect };
