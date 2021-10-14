1aab27d3a7fd52701dc3201d1d0fd41f54cda263
ZachHung <zachhung911@gmail.com>
2021-10-14 20:46:53 +0700

    Fixing indentation

--- src/app/controllers/PhoneController.js
+++ src/app/controllers/PhoneController.js
@@ -1,17 +1,17 @@
-const items = require('../models/Item')
+const items = require("../models/Item");
 
 class PhoneController {
-    index(req, res,next) {
-    items.find({})
-        .then(items => {
-            items = items.map((item) => item.toObject())
-            res.render('phone',{items})
-        }
-        )
-        .catch(next)
- }
-   show(req, res){
-       res.send('home' + req.params.id);
-   }
+    index(req, res, next) {
+        items
+            .find({})
+            .then((items) => {
+                items = items.map((item) => item.toObject());
+                res.render("phone", { items });
+            })
+            .catch(next);
+    }
+    show(req, res) {
+        res.send("home" + req.params.id);
+    }
 }
-module.exports = new PhoneController ;
+module.exports = new PhoneController();
