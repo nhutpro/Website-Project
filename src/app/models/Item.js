399f18d7777b273c3347a175b4d7b6aa30964730
ZachHung <zachhung911@gmail.com>
2021-10-16 13:05:42 +0700

    Fix identation

--- src/app/models/Item.js
+++ src/app/models/Item.js
@@ -1,11 +1,11 @@
-const mongoose =require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const Item = new Schema({
-  name: String,
-  type: String,
-  image: String,
-  price: String ,
-  description: String
+    name: String,
+    type: String,
+    image: String,
+    price: String,
+    description: String,
 });
-module.exports = mongoose.model('Item', Item)
+module.exports = mongoose.model("Item", Item);
