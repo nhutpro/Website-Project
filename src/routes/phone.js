31adc8c402ff75aa49908ad71f6335fdbf262a98
nhutpro <tnhut803@gmail.com>
2021-10-31 22:32:47 +0700

    update

--- src/routes/phone.js
+++ src/routes/phone.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const itemController = require('../app/controllers/ItemController');
 const phoneController = require('../app/controllers/PhoneController');
 
-router.get('/:slug', phoneController.show);
+router.get('/:slug', itemController.detailItem);
 router.get('/', phoneController.index);
 
 module.exports = router;
