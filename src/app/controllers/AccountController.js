b012505ce12bfd7bd0711742f4e068e2f8d30e05
nhutpro <tnhut803@gmail.com>
2021-11-21 20:13:45 +0700

    account

--- src/app/controllers/AccountController.js
+++ src/app/controllers/AccountController.js
@@ -1,6 +1,63 @@
 const nodemailer = require("nodemailer");
-const users = require("../models/User");
+const user = require("../models/Users");
+var recoveryCode = 9450;
+var confirmCode;
+var emailRecovery = "tnhut803@gmail.com";
+let transporter = nodemailer.createTransport({
+	service: "gmail",
+	auth: {
+		user: "nhutt460@gmail.com", // generated ethereal user
+		pass: "Trannhut1", // generated ethereal password
+	},
+});
+function getRandom(min, max) {
+	return Math.round(Math.random() * (max - min) + min);
+}
+function sendMail(desMail, Message) {
+	var code = getRandom(1000, 10000);
+	transporter.sendMail({
+		from: "nhutt460@gmail.com", // sender address
+		to: `${desMail}`, // list of receivers
+		subject: "Mã Xác Thực Gmail", // Subject line
+		text: `${Message} ${code}`, // plain text body
+	});
+	return code;
+}
 class AccountController {
+	register(req, res, next) {
+		confirmCode = sendMail(req.body.email, "Mã Xác Thực gmail của bạn là :");
+		console.log(confirmCode);
+		res.send(true);
+	}
+	registerConfirm(req, res, next) {
+		if (req.body.code === `${confirmCode}`) {
+			user
+				.create({
+					email: req.body.email,
+					password: req.body.password,
+					phone: req.body.phone,
+					name: req.body.name,
+					address: "",
+				})
+				.then((model) => {
+					res.send({
+						status: "true",
+					});
+					console.log(model);
+				})
+				.catch((err) => {
+					res.send({
+						status: "false",
+						message: "Lỗi database vui lòng nhập lại mã",
+					});
+				});
+		} else {
+			res.send({
+				status: "false",
+				message: "Mã Xác Minh Không Chính Xác",
+			});
+		}
+	}
 	login(req, res, next) {
 		const loginInfo = req.body;
 		let email = loginInfo.email;
@@ -15,14 +72,9 @@ class AccountController {
 						user: "",
 					});
 				} else {
-					res.send({
-						login: true,
-						user: users[0],
-					});
+					res.send(true);
 					req.session.user = users[0];
 				}
-				console.log(users[0]);
-				console.log(users.length);
 			})
 			.catch(next);
 	}
@@ -32,20 +84,45 @@ class AccountController {
 		console.log(req.session.user);
 	}
 	recovery(req, res, next) {
-		let transporter = nodemailer.createTransport({
-			service: "gmail",
-			auth: {
-				user: "nhutt460@gmail.com", // generated ethereal user
-				pass: "Trannhut1", // generated ethereal password
-			},
-		});
-		let info = transporter.sendMail({
-			from: "nhutt460@gmail.com", // sender address
-			to: "tnhut803@gmail.com", // list of receivers
-			subject: "Khôi Phục Tài Khoản", // Subject line
-			text: "Mã thay đổi mật khẩu của bạn là 2345", // plain text body
-		});
-		res.send("Nhutpro");
+		recoveryCode = sendMail(req.body.email, "Mã Khôi Phục Của Bạn Là: ");
+		console.log(recoveryCode);
+		res.send(false);
+	}
+	recoveryConfirm(req, res, next) {
+		console.log([req.body.email, recoveryCode]);
+		console.log(req.body);
+		if (req.body.code == recoveryCode) {
+			emailRecovery = req.body.email;
+			res.send({
+				status: "true",
+			});
+		} else {
+			res.send({
+				status: "false",
+				message: "Mã Xác Minh Không Chính Xác",
+			});
+		}
+	}
+	recoveryUpdate(req, res, next) {
+		user
+			.updateMany(
+				{
+					email: `${emailRecovery}`,
+				},
+				{
+					password: `${req.body.password}`,
+				}
+			)
+			.then((data) => {
+				res.send({
+					status: "true",
+				});
+			})
+			.catch((err) => {
+				res.send({
+					status: "false",
+				});
+			});
 	}
 }
 module.exports = new AccountController();
--- src/public/javs/HeFo.js
+++ src/public/javs/HeFo.js
@@ -3,11 +3,15 @@ const Forms = document.querySelectorAll(".account-modal-container form");
 const submitBtns = document.querySelectorAll(
 	".account-modal-container input[type ='submit'] "
 );
+
+/*
 submitBtns.forEach((item) => {
 	item.addEventListener("click", (e) => {
 		e.preventDefault();
 	});
 });
+*/
+
 Forms.forEach((item) => {
 	item.addEventListener("submit", (e) => {
 		e.preventDefault();
@@ -18,9 +22,16 @@ Forms.forEach((item) => {
 /*start open account form */
 const loginForm = document.querySelector(".account-modal .login-form");
 const signUpForm = document.querySelector(".account-modal .signUp-form");
+const signUpFormConfirm = document.querySelector(
+	".account-modal .signUp-form-confirm"
+);
 const findAccountForm = document.querySelector(
 	".account-modal .find-account-form"
 );
+const findAccountFormChange = document.querySelector(
+	".account-modal .find-account-form-change"
+);
+
 const accountIcon = document.querySelector("header .icon.account");
 const accountModal = document.querySelector(".account-modal-container");
 accountIcon.addEventListener("click", (e) => {
@@ -28,8 +39,31 @@ accountIcon.addEventListener("click", (e) => {
 	loginForm.style.display = "block";
 	signUpForm.style.display = "none";
 	findAccountForm.style.display = "none";
+	resetModal();
 });
 /*end account form */
+/*start function reset modal */
+function resetModal() {
+	var errorMessages = document.querySelectorAll(
+		".account-modal-container .error-message"
+	);
+	for (var message of errorMessages) {
+		message.innerText = "";
+		message.parentElement
+			.querySelector(".form-row")
+			.classList.remove("invalid");
+	}
+	const inputPassword = document.querySelectorAll(
+		".account-modal-container input[name *='assword']"
+	);
+	for (var input of inputPassword) {
+		input.setAttribute("type", "password");
+		input.parentElement
+			.querySelector(".cover-icon")
+			.setAttribute("class", "fas fa-eye-slash cover-icon");
+	}
+}
+/*end function resen modal  */
 
 /*start close account form */
 const accountFormCloseBtns = document.querySelectorAll(
@@ -74,13 +108,14 @@ const forwardSignUpBtn = document.querySelector(
 const forwardFindAccountBtn = document.querySelector(
 	".account-modal .forward-FindAccount"
 );
-console.log([forwardFindAccountBtn, findAccountForm]);
+
 forwardLoginBtns.forEach((btn) => {
 	btn.addEventListener("click", (e) => {
 		currentForm = e.target.parentNode;
 		currentForm.reset();
 		currentForm.style.display = "none";
 		loginForm.style.display = "block";
+		resetModal();
 	});
 });
 forwardSignUpBtn.addEventListener("click", (e) => {
@@ -88,41 +123,247 @@ forwardSignUpBtn.addEventListener("click", (e) => {
 	currentForm.reset();
 	currentForm.style.display = "none";
 	signUpForm.style.display = "block";
+	resetModal();
 });
 forwardFindAccountBtn.addEventListener("click", (e) => {
 	currentForm = e.target.parentNode;
 	currentForm.reset();
 	currentForm.style.display = "none";
 	findAccountForm.style.display = "block";
+	resetModal();
 });
+
+/* start solve error from backend */
+function errSever(input, message) {
+	input.value = "";
+	input.parentElement.classList.add("invalid");
+	input.parentElement.parentElement.querySelector(".error-message").innerText =
+		message;
+}
+/* end solve error from backend */
 
 /* end forward all form */
 
-/* start click login submit button */
-function sendUserInfo() {
-	const loginEmail = document.getElementById("login-mail");
-	const loginPassWord = document.getElementById("login-password");
-	//start fetch //
-	console.log("start send data");
+/*start solve signUp form*/
+
+//start solve submit signUp form //
+function signUpSubmit(data) {
+	fetch("http://localhost:3000/account/register", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify(data),
+	})
+		.then((res) => {
+			if (res) {
+				signUpForm.reset();
+				signUpForm.style.display = "none";
+				signUpFormConfirm.style.display = "block";
+
+				//start solve signUp confirm
+				Validator({
+					form: ".signUp-form-confirm",
+					errorSelector: ".error-message",
+					rules: [Validator.isRequired("#signUp-code")],
+					onSubmit: function (code) {
+						fetch("http://localhost:3000/account/register/confirm", {
+							method: "POST",
+							headers: {
+								"Content-Type": "application/json",
+							},
+							body: JSON.stringify({
+								...data,
+								...code,
+							}),
+						})
+							.then((res) => res.json())
+							.then((res) => {
+								if (res.status === "true") {
+									signUpFormConfirm.style.display = "none";
+									signUpFormConfirm.reset();
+									loginForm.style.display = "block";
+									resetModal();
+								} else {
+									input = document.querySelector(
+										".signUp-form-confirm #signUp-code"
+									);
+									errSever(input, res.message);
+								}
+							})
+							.catch((err) => {
+								console.log(err);
+							});
+					},
+				});
+				//end solve signUp confirm
+			}
+		})
+		.catch((error) => {
+			console.log(error);
+		});
+}
+//start solve submit signUp form //
+//start check valid sign up form//
+Validator({
+	form: ".signUp-form",
+	errorSelector: ".error-message",
+	rules: [
+		Validator.isRequired("#signUp-fullname"),
+		Validator.isRequired("#signUp-email"),
+		Validator.isEmail("#signUp-email"),
+		Validator.isRequired("#signUp-password"),
+		Validator.isPhone("#signUp-phone"),
+		Validator.isMinLength("#signUp-password", 6),
+		Validator.isRequired("#signUp-confirmPassword"),
+		Validator.isConfirm("#signUp-confirmPassword", function () {
+			return document.querySelector(".signUp-form #signUp-password").value;
+		}),
+	],
+	onSubmit: function (data) {
+		signUpSubmit(data);
+	},
+});
+//start check valid sign up form
+
+/*end solve signUp form*/
+
+/*star solve login form*/
+//start solve submit login form //
+function loginSubmit(data) {
 	fetch("http://localhost:3000/account/login", {
 		method: "POST",
 		headers: {
 			"Content-Type": "application/json",
 		},
-		body: JSON.stringify({
-			email: `${loginEmail.value}`,
-			password: `${loginPassWord.value}`,
-		}),
+		body: JSON.stringify(data),
 	})
-		.then((data) => data.json())
-		.then((data) => {
-			console.log(data);
-			console.log("success");
+		.then((res) => {
+			if (res) {
+				document.querySelector(".login-form .modal-close-btn").click();
+			}
+		})
+		.catch((res) => {});
+}
+//end solve submit login form//
+Validator({
+	form: ".login-form",
+	errorSelector: ".error-message",
+	rules: [
+		Validator.isEmail("#login-mail"),
+		Validator.isMinLength("#login-password", 6),
+	],
+	onSubmit: function (data) {
+		loginSubmit(data);
+	},
+});
+/*end solve login form*/
+
+/*start solve recovery form */
+function checkEmail(value) {
+	var regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+	return regex.test(value) ? undefined : "Trường này phải là email";
+}
+var getCodeBtn = document.querySelector(".find-account-form .get-code-btn");
+var inputEmail = document.querySelector(".find-account-form #recovery-email");
+var recoveryMessageError =
+	inputEmail.parentElement.parentElement.querySelector(".error-message");
+/* start get recovery code */
+getCodeBtn.addEventListener("click", (e) => {
+	var errorMessage = checkEmail(inputEmail.value);
+	e.preventDefault();
+	if (errorMessage) {
+		recoveryMessageError.innerText = errorMessage;
+	} else {
+		fetch("http://localhost:3000/account/recovery", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({
+				email: inputEmail.value,
+			}),
+		})
+			.then((res) => res.json())
+			.then((res) => {
+				if (res == "false") {
+				} else {
+				}
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	}
+});
+/* end get recovery code */
+/*start sovle recoveryform submit */
+
+function recoverySubmit(recoveryData) {
+	fetch("http://localhost:3000/account/recovery/confirm", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify(recoveryData),
+	})
+		.then((res) => res.json())
+		.then((res) => {
+			if (res.status == "true") {
+				{
+					findAccountForm.reset();
+					findAccountForm.style.display = "none";
+					findAccountFormChange.style.display = "block";
+					resetModal();
+				}
+			} else {
+				input = document.querySelector(".find-account-form #recovery-code");
+				errSever(input, res.message);
+			}
 		});
 }
 
-const loginSubmit = document.querySelector(
-	".account-modal-container .login-submit"
-);
-loginSubmit.addEventListener("click", sendUserInfo);
-/* end click login submit button */
+/*end sovle recoveryform submit */
+Validator({
+	form: ".find-account-form",
+	errorSelector: ".error-message",
+	rules: [
+		Validator.isEmail("#recovery-email"),
+		Validator.isRequired("#recovery-code"),
+	],
+	onSubmit: function (recoveryData) {
+		recoverySubmit(recoveryData);
+	},
+});
+
+function ChangePassSubmit(password) {
+	fetch("http://localhost:3000/account/recovery/update", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify(password),
+	})
+		.then((res) => res.json())
+		.then((res) => {
+			if (res.status == "true") {
+				findAccountFormChange.style.display = "none";
+				findAccountFormChange.reset();
+				resetModal();
+				loginForm.style.display = "block";
+			} else {
+			}
+		});
+}
+
+Validator({
+	form: ".find-account-form-change",
+	errorSelector: ".error-message",
+	rules: [
+		Validator.isRequired("#recovery-confirmPassword"),
+		Validator.isRequired("#recovery-password"),
+	],
+	onSubmit: function (password) {
+		ChangePassSubmit(password);
+	},
+});
+/*end solve recovery form */
