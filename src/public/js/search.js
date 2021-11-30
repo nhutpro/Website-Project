d0fa830842406b8543b1ea30be356d3d749b27c5
Your name <hoan42532@gmail.com>
2021-11-30 22:18:03 +0700

    fixed null setting properties in search.js

--- src/public/js/search.js
+++ src/public/js/search.js
@@ -1,14 +1,15 @@
-var searchProduct = document.getElementById('searchBox')
-// var countProduct = document.querySelectorAll('div[id=countProduct]')
-var count = document.getElementById('count')
-var key = document.getElementById('key')
-var sortAsc = document.getElementById('sort-asc')
-var sortDesc = document.getElementById('sort-desc')
+
 // localStorage.setItem("match", 0)
 
 
 
 window.onload = () => {
+    var searchProduct = document.getElementById('searchBox')
+    // var countProduct = document.querySelectorAll('div[id=countProduct]')
+    var count = document.getElementById('count')
+    var key = document.getElementById('key')
+    var sortAsc = document.getElementById('sort-asc')
+    var sortDesc = document.getElementById('sort-desc')
     searchProduct.onchange = () => {
         var api = "http://localhost:3000/search/match?key=" + searchProduct.value
         fetch(api, {
@@ -30,19 +31,20 @@ window.onload = () => {
 
     }
 
-    count.innerHTML = localStorage.getItem("match")
 
 
-    key.innerHTML = "'" + localStorage.getItem("count") + "'"
-    // sort price
-    sortAsc.onclick = () => {
-        window.history.pushState({}, "", "http://localhost:3000/search/global?key=" + localStorage.getItem("count") + "&sort=asc");
-        window.location.reload();
-    }
-    sortDesc.onclick = () => {
-        window.history.pushState({}, "", "http://localhost:3000/search/global?key=" + localStorage.getItem("count") + "&sort=desc");
-        window.location.reload();
-    }
+    // count.innerHTML = localStorage.getItem("match")
+
+    // key.innerHTML = "'" + localStorage.getItem("count") + "'"
+    // // sort price
+    // sortAsc.onclick = () => {
+    //     window.history.pushState({}, "", "http://localhost:3000/search/global?key=" + localStorage.getItem("count") + "&sort=asc");
+    //     window.location.reload();
+    // }
+    // sortDesc.onclick = () => {
+    //     window.history.pushState({}, "", "http://localhost:3000/search/global?key=" + localStorage.getItem("count") + "&sort=desc");
+    //     window.location.reload();
+    // }
 
 }
 
