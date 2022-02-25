804b33f2bdf43ff6889d05a4c4ad746ec46f3fd9
ZachHung <zachhung911@gmail.com>
2022-02-25 22:29:45 +0700

    Add gzip compression

--- src/index.js
+++ src/index.js
@@ -8,9 +8,11 @@ const path = require("path");
 const app = express();
 const port = 3000;
 const route = require("./routes");
+const compression = require("compression");
 require("dotenv").config();
 
 app.use(express.static(path.join(__dirname, "public")));
+app.use(compression());
 app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
