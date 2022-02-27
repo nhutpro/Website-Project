629278ff752cf93b8267cb72fa38de007a03ede1
ZachHung <zachhung911@gmail.com>
2022-02-27 08:45:08 +0700

    change https redirect from 302 to 301

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ require("dotenv").config();
 if (process.env.NODE_ENV === "production") {
   app.use((req, res, next) => {
     if (req.header("x-forwarded-proto") !== "https")
-      res.redirect(`https://${req.header("host")}${req.url}`);
+      res.redirect(301, `https://${req.header("host")}${req.url}`);
     else next();
   });
 }
